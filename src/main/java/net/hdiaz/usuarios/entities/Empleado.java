59c92e53d422e0a492c4bb0fab6e6abc5c45532e
Herman Diaz Luna <herosdial@hotmail.com>
2024-10-09 16:55:13 -0500

    Agregando la tabla empleados

---
+++ src/main/java/net/hdiaz/usuarios/entities/Empleado.java
@@ -0,0 +1,58 @@
+package net.hdiaz.usuarios.entities;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Empleado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String cedula;
+    private String nombre;
+    private String telefono;
+
+    public Empleado(Long id, String cedula, String nombre, String telefono) {
+        this.id = id;
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public Empleado(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
